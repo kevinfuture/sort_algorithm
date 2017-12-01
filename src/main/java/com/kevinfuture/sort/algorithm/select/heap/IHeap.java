28d4debe3add2338eb7bbf76420e61542917c83c
kevinfuture <liuenbofuture@foxmail.com>
2017-12-01 14:49:30 +0800

    选择排序

--- src/main/java/com/kevinfuture/sort/algorithm/select/heap/IHeap.java
+++ src/main/java/com/kevinfuture/sort/algorithm/select/heap/IHeap.java
@@ -1,5 +1,9 @@
 package com.kevinfuture.sort.algorithm.select.heap;
 
+import com.kevinfuture.sort.algorithm.enums.impl.OrderEnum;
+
+import java.util.List;
+
 /**
  * 堆排序
  *
@@ -7,4 +11,12 @@ package com.kevinfuture.sort.algorithm.select.heap;
  * @date 2017/11/30
  */
 public interface IHeap {
+    /**
+     * 堆排序接口方法
+     * @param doubles
+     * @param orderEnum
+     * @return list
+     * @throws Throwable
+     ***/
+    List sort(double[] doubles, OrderEnum orderEnum) throws Throwable;
 }
---
+++ src/main/java/com/kevinfuture/sort/algorithm/select/heap/impl/HeapImpl.java
@@ -0,0 +1,18 @@
+package com.kevinfuture.sort.algorithm.select.heap.impl;
+
+import com.kevinfuture.sort.algorithm.enums.impl.OrderEnum;
+import com.kevinfuture.sort.algorithm.select.heap.AbstractHeap;
+
+import java.util.List;
+
+/**
+ * 堆排序的实现
+ * @author kevinfuture
+ * @date 2017/12/1
+ */
+public class HeapImpl extends AbstractHeap{
+    @Override
+    public List sort(double[] doubles, OrderEnum orderEnum) throws Throwable {
+        return null;
+    }
+}
